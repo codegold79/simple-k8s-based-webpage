9be1d07b9073bafe3720bd4a0992d1e8732ffd93
F. Gold <fgold@vmware.com>
2020-09-22 14:54:36 -0700

    Use env var to set response status code
    Signed-off-by: F. Gold <fgold@vmware.com>

--- server.go
+++ server.go
@@ -5,26 +5,58 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
 
+// Requests stores the amount of time to sleep before a response
+// as well as the status code to return.
 type Requests struct {
 	SleepTime, StatusCode int
 }
 
 func main() {
-	http.HandleFunc("/300/", statusRedirectHandler)
-	http.HandleFunc("/200/", statusOkHandler)
-	http.HandleFunc("/400/", statusUserErrHandler)
-	http.HandleFunc("/500/", statusServerErrHandler)
+	http.HandleFunc("/", envStatusHandler)
+	http.HandleFunc("/200/", status200Handler)
+	http.HandleFunc("/300/", status300Handler)
+	http.HandleFunc("/400/", status400Handler)
+	http.HandleFunc("/500/", status500Handler)
 	http.HandleFunc("/favicon.ico", faviconHandler)
 
-	fmt.Println("Serving...")
+	fmt.Println("Serving on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func statusOkHandler(w http.ResponseWriter, r *http.Request) {
+func envStatusHandler(w http.ResponseWriter, r *http.Request) {
+	code := validCode()
+
+	req := Requests{
+		SleepTime:  0,
+		StatusCode: code,
+	}
+
+	if code > 0 {
+		w.WriteHeader(code)
+	}
+
+	executeTemplate(w, req)
+}
+
+// validCode grabs an environmental variable and stores it as an integer.
+func validCode() int {
+	envCode := os.Getenv("STATUSCODE")
+	code, err := strconv.Atoi(envCode)
+
+	if err != nil {
+		log.Printf("STATUSCODE env var needs to be an integer between 200-599, inclusive: %v\n", err)
+		return -1
+	}
+
+	return code
+}
+
+func status200Handler(w http.ResponseWriter, r *http.Request) {
 	var sleepTime int
 
 	sleepTimes, ok := r.URL.Query()["sleep"]
@@ -37,7 +69,7 @@ func statusOkHandler(w http.ResponseWriter, r *http.Request) {
 	executeTemplate(w, req)
 }
 
-func statusRedirectHandler(w http.ResponseWriter, r *http.Request) {
+func status300Handler(w http.ResponseWriter, r *http.Request) {
 	var sleepTime int
 
 	w.WriteHeader(http.StatusMultipleChoices)
@@ -52,32 +84,32 @@ func statusRedirectHandler(w http.ResponseWriter, r *http.Request) {
 	executeTemplate(w, req)
 }
 
-func statusUserErrHandler(w http.ResponseWriter, r *http.Request) {
+func status400Handler(w http.ResponseWriter, r *http.Request) {
 	var sleepTime int
 
-	w.WriteHeader(http.StatusTeapot)
+	w.WriteHeader(http.StatusBadRequest)
 
 	sleepTimes, ok := r.URL.Query()["sleep"]
 	if ok {
 		sleepTime = waitForSleep(w, sleepTimes)
 	}
 
-	req := Requests{sleepTime, 418}
+	req := Requests{sleepTime, 400}
 
 	executeTemplate(w, req)
 }
 
-func statusServerErrHandler(w http.ResponseWriter, r *http.Request) {
+func status500Handler(w http.ResponseWriter, r *http.Request) {
 	var sleepTime int
 
-	w.WriteHeader(http.StatusInsufficientStorage)
+	w.WriteHeader(http.StatusInternalServerError)
 
 	sleepTimes, ok := r.URL.Query()["sleep"]
 	if ok {
 		sleepTime = waitForSleep(w, sleepTimes)
 	}
 
-	req := Requests{sleepTime, 507}
+	req := Requests{sleepTime, 500}
 
 	executeTemplate(w, req)
 }
